95a90278c20855d2d99600e7757103a638c9cff5
kb21032001 <kshitij.bhardwaj21@gmail.com>
2023-10-16 02:19:08 -0400

    Implemented logout system

--- app/home.py
+++ app/home.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, render_template
+from auth import login_required
 
 home = Blueprint('home', __name__)
 
 @home.route('/user', methods=['GET'])
+@login_required
 def user_main():
     return render_template('user_main.html')
 
 @home.route('/organizer', methods=['GET'])
+@login_required
 def organizer_main():
     return render_template('organizer_main.html')
