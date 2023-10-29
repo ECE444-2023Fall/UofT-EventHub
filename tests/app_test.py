50c39c6727b175ec334a530a280ea5c0c4025fd6
Rahul Chopra <98198008+rahuljchopra@users.noreply.github.com>
2023-10-29 09:59:18 -0400

    Created app_test.py for unit tests

---
+++ tests/app_test.py
@@ -0,0 +1,42 @@
+import pytest
+from app.main import app, db
+from pathlib import Path
+
+TEST_DB = "test.db"
+
+# Test function written by Rahul
+@pytest.fixture
+def client():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    app.config["TESTING"] = True
+    app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
+
+    with app.app_context():
+        db.create_all()
+        yield app.test_client()
+        db.drop_all()
+
+# Test function written by Rahul
+# Test to ensure that the database exists
+def test_database(client):
+    tester = Path("test.db").is_file()
+    assert tester
+
+# Test function written by Rahul
+# Test if home page is accessible
+def test_index():
+    tester = app.test_client()
+    response = tester.get("/", content_type="html/text")
+    assert response.status_code == 200
+
+# Test function written by Rahul
+# Test if user can register
+def test_register_success(client):
+    response = client.post("/register", data=dict(username="user123",
+        password1="PaSsWoRd",
+        password2="PaSsWoRd",
+        role="user"),
+        follow_redirects=True)
+    assert response.status_code == 200
+    assert b"Account created!" in response.data
