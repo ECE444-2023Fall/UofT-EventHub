bffe0308b18fbfb91b3a7e2edcb5ffd1f7c6421c
rahuljchopra <choprarahul00@gmail.com>
2023-11-03 18:58:12 -0400

    Added functionality to add events to Google Calendar with temporary information. Will add reading from database.

--- app/events.py
+++ app/events.py
@@ -2,6 +2,13 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for,
 from flask_login import login_required, current_user
 from flask import current_app
 import os
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 from app.main import db
 from app.database import EventDetails
@@ -30,3 +37,45 @@ def send_file(filename):
     """
 
     return send_from_directory(current_app.config["GRAPHIC_DIRECTORY"], filename)
+
+def create_google_calendar_event():
+    SCOPES = ['https://www.googleapis.com/auth/calendar']
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        event = {
+            "summary": "My ECE444 Event",
+            "location": "room BA1160",
+            "colorId": 6,
+            "start": {
+                "dateTime": "2023-12-02T09:00:00",
+                "timeZone": "Canada/Eastern"
+            },
+            "end": {
+                "dateTime": "2023-12-02T17:00:00",
+                "timeZone": "Canada/Eastern"
+            }
+        }
+
+        event = service.events().insert(calendarId="primary", body=event).execute()
+        print(f'Event created: {event.get("htmlLink")}')
+
+    except HttpError as error:
+        print("An error occurred:", error)
