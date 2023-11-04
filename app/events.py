4e128ba3e98fbd6c146d958fd8e237351fd3b766
rahuljchopra <choprarahul00@gmail.com>
2023-11-03 21:46:07 -0400

    Updated call to Google credentials

--- app/events.py
+++ app/events.py
@@ -52,7 +52,7 @@ def create_google_calendar_event():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+                'Google_Calendar/credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
