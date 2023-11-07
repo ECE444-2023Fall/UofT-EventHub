ded12ca2877d8304f2d5b73c87710c9ac5df63bf
rahuljchopra <choprarahul00@gmail.com>
2023-11-07 17:43:48 -0500

    Created a route for users to view all the organizers

--- app/user.py
+++ app/user.py
@@ -2,10 +2,12 @@ from flask import Blueprint, render_template
 
 from app.globals import FILTERS
 from app.auth import login_required, user_required
-from app.database import EventDetails
+from app.database import EventDetails, OrganizerEventDetails
 from app.search import get_eventids_matching_search_query
 from app.filter import filter_for_today_events, filter_for_inperson_events, filter_for_free_events, filter_events_on_category, filter_events_on_event_ids_list
+from app.globals import Role
 
+from sqlalchemy import distinct
 
 user = Blueprint("user", __name__)
 
@@ -38,15 +40,40 @@ def main(filter="all", search=None):
 def get_all_events_from_database():
     events_data = EventDetails.query.all()
 
-    ## Make a dict for event details
+    # Make a dict for event details
     dict_of_events_details = {}
     for row in events_data:
         event_detail = {}
 
-        ## TODO: Ideally we should only be passing information that is required by the user_main.html
+        # TODO: Ideally we should only be passing information that is required by the user_main.html
         for column in row.__table__.columns:
             event_detail[column.name] = str(getattr(row, column.name))
 
         dict_of_events_details[row.id] = event_detail
 
     return dict_of_events_details
+
+
+@user.route("/user/organizers", methods=["GET"])
+@login_required
+@user_required
+def view_organizers():
+    organizer_usernames = get_organizers()
+    
+    # Create a string with organizer usernames as headings
+    organizer_headings = "<h1>Organizer Usernames:</h1>"
+    for username in organizer_usernames:
+        organizer_headings += f"<h2>{username}</h2>"
+    
+    return organizer_headings
+
+# Get all organizers that have/had upcoming/past events
+def get_organizers():
+    
+    # Query the database to get a list of all organizer usernames with upcoming/past events
+    organizers = OrganizerEventDetails.query.with_entities(distinct(OrganizerEventDetails.organizer_username)).all()
+    
+    # Extracting the usernames from the query results
+    organizer_usernames = [organizer[0] for organizer in organizers]
+    
+    return organizer_usernames
