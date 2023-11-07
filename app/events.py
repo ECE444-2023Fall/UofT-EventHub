2d3d0b4db4874e882d82501bb80f07aedb330dc0
Fabin Joe <fabin2005@outlook.com>
2023-11-07 14:52:34 -0500

    Created an admin page for events
    - Added ability to edit events
    - Added an option to display analytics for an event (Currently, the only analytics passed is no of attendees)

--- app/events.py
+++ app/events.py
@@ -11,11 +11,13 @@ from flask import (
 from flask_login import login_required, current_user
 from sqlalchemy import delete
 import logging
+import os
 
-from app.main import db
+from app.main import db, es  # db is for database and es for elastic search
 from app.globals import Role
-from app.auth import login_required
-from app.database import Credentials, EventRegistration, EventDetails
+from app.auth import login_required, organizer_required
+from app.database import Credentials, EventRegistration, EventDetails, EventBanner
+from app.forms import EventCreateForm
 
 events = Blueprint("events", __name__)
 
@@ -41,6 +43,151 @@ def show_event(id):
     else:
         return render_template("event.html", event=event.__dict__, is_registered=False)
 
+@events.route("/events/admin/<int:id>", methods=["GET"])
+@login_required
+@organizer_required
+def show_event_admin(id):
+    logging.info("Loading admin webpage for event ID: %d", id)
+
+    ## Get all the details for the event
+    event = EventDetails.query.filter_by(id=id).first()
+
+    if not event:
+        print(
+            "Integrity Error: The event ID passed to show_event_admin has no valid entry in the database"
+        )
+
+    # Get some analytics data for this event
+    registered_users = EventRegistration.query.filter_by(event_id=id).all()
+    num_of_registrations = len(registered_users)
+
+    return render_template("event_admin.html", event=event.__dict__, num_of_registrations=num_of_registrations)
+
+@events.route("/events/create_event", methods=["GET", "POST"])
+@login_required
+@organizer_required
+def create_event():
+    form = EventCreateForm()
+
+    if form.validate_on_submit():
+        # Add the event details
+        new_event = EventDetails(
+            name=form.name.data,
+            description=form.description.data,
+            category=form.category.data.lower(),
+            organizer=current_user.username,
+            is_online=form.is_online.data,
+            venue=form.venue.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data,
+            start_time=form.start_time.data,
+            end_time=form.end_time.data,
+            max_capacity=form.max_capacity.data,
+            current_capacity=0,
+            ticket_price=form.ticket_price.data,
+            redirect_link=form.redirect_link.data,
+            additional_info=form.additional_info.data,
+        )
+        db.session.add(new_event)
+        db.session.commit()
+
+        add_event_to_index(new_event)
+
+        # Add the banner information for the newly created event
+        banner_file = form.banner_image.data
+        filename = "event_banner_" + str(new_event.id) + ".png"
+
+        # Save the banner in assets/event-assets
+        banner_file.save(
+            os.path.join(current_app.root_path, "assets", "event-assets", filename)
+        )
+
+        # Store the path to the banner EventBanner
+        event_graphic = EventBanner(event_id=new_event.id, image=filename)
+
+        db.session.add(event_graphic)
+        db.session.commit()
+
+        return redirect(url_for("events.show_event_admin", id=new_event.id))
+
+    organizer = current_user
+
+    return render_template("create_event.html", form=form)
+
+@events.route("/events/edit_event/<int:id>", methods=["GET", "POST"])
+@login_required
+@organizer_required
+def edit_event(id):
+    logging.info("Loading edit event webpage for event ID: %d", id)
+
+    ## Get all the details for the event
+    event = EventDetails.query.filter_by(id=id).first()
+
+    if not event:
+        print(
+            "Integrity Error: The event ID passed to show_event_admin has no valid entry in the database"
+        )
+
+    # Recreate the Event Form with the default values set
+    event_create_form_default = {}
+    for field_name, default_value in event.__dict__.items():
+        if not hasattr(EventCreateForm, field_name):
+            logging.info(f"Form doesn't have attribute: {field_name}")   
+            continue
+        # NOTE: Since we are decapitilizing the category data on form submition, 
+        # we must perform the opposite operation here
+        if field_name == "category":
+            default_value = default_value.capitalize()
+        event_create_form_default[field_name] = default_value
+    logging.info(f"Default Values: {event_create_form_default}")   
+
+    form = EventCreateForm(**event_create_form_default)
+
+    if form.validate_on_submit():
+        logging.info(f"Edited Event Entries: {form.name.data}, {form.category.data}")
+        # Add the event details
+        event.name=form.name.data
+        event.description=form.description.data
+        event.category=form.category.data.lower()
+        event.is_online=form.is_online.data
+        event.venue=form.venue.data
+        event.start_date=form.start_date.data
+        event.end_date=form.end_date.data
+        event.start_time=form.start_time.data
+        event.end_time=form.end_time.data
+        # TODO: Add a check to prevent the max capacity from being set to below the current capacity
+        event.max_capacity=form.max_capacity.data
+        event.ticket_price=form.ticket_price.data
+        event.redirect_link=form.redirect_link.data
+        event.additional_info=form.additional_info.data
+        db.session.commit()
+
+        # TODO: Update the event details to the index for elastic search
+
+        # Add the banner information for the newly created event
+        banner_file = form.banner_image.data
+        filename = "event_banner_" + str(event.id) + ".png"
+
+        # Save the banner in assets/event-assets
+        banner_file.save(
+            os.path.join(current_app.root_path, "assets", "event-assets", filename)
+        )
+
+        # Store the path to the banner EventBanner
+        event_graphic = EventBanner.query.filter_by(event_id=event.id).first()
+        if event_graphic:
+            event_graphic.image = filename
+        else:
+            event_graphic = EventBanner(event_id=event.id, image=filename)
+            db.session.add(event_graphic)
+
+        db.session.commit()
+
+        return redirect(url_for("events.show_event_admin", id=event.id))
+
+    organizer = current_user
+
+    return render_template("create_event.html", form=form)
 
 @events.route("/events/send_file/<filename>")
 @events.route("/events/register/send_file/<filename>")
@@ -121,3 +268,17 @@ def register_for_event(event_id):
         logging.info("Value: %s", val)
     return render_template("event.html", event=event.__dict__, is_registered=True)
     
+def add_event_to_index(new_event):
+    event_detail = {
+        "id": new_event.id,
+        "name": new_event.name,
+        "description": new_event.description,
+        "category": new_event.category,
+        "venue": new_event.venue,
+        "additional_info": new_event.additional_info,
+    }
+
+    logging.info("The event dict for indexing:", event_detail)
+    es.index(index="events", document=event_detail)
+    es.indices.refresh(index="events")
+    logging.info(es.cat.count(index="events", format="json"))
--- app/globals.py
+++ app/globals.py
@@ -5,7 +5,7 @@ class Role(Enum):
     USER = 0
     ORGANIZER = 1
 
-EVENT_CATEGORIES = ["Music", "Nightlife", "Hobbies", "Personal Development", "Business"]
+EVENT_CATEGORIES = ["Academic", "Hobbies", "Music", "Nightlife", "Business"]
 # List of tags that can be used to sort the event data
 # NOTE: Do not add a tag named "clear", since it has a special functionality
 FILTERS = ["In-Person", "Today", "Free", *EVENT_CATEGORIES]
