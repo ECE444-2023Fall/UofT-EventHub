83c9bfd5bd9f7041a77cf4e943c05fab23607d21
rahuljchopra <choprarahul00@gmail.com>
2023-11-08 03:16:26 -0500

    Updated users view of organizers page. Added list of organizers names

--- app/user.py
+++ app/user.py
@@ -1,14 +1,12 @@
 from flask import Blueprint, render_template
+from sqlalchemy import distinct
 
 from app.globals import FILTERS
 from app.auth import login_required, user_required
-from app.database import EventDetails, OrganizerEventDetails
+from app.database import EventDetails, Credentials
 from app.search import get_eventids_matching_search_query
 from app.filter import filter_for_today_events, filter_for_inperson_events, filter_for_free_events, filter_events_on_category, filter_events_on_event_ids_list
 from app.main import db
-
-
-from sqlalchemy import distinct
 
 user = Blueprint("user", __name__)
 
@@ -55,49 +53,20 @@ def get_all_events_from_database():
     return dict_of_events_details
 
 
-"""@user.route("/user/organizers", methods=["GET"])
-@login_required
-@user_required
-def view_organizers():
-    organizer_usernames = get_organizers()
-    
-    # Create a string with organizer usernames as headings
-    organizer_headings = "<h1>Organizer Usernames:</h1>"
-    for username in organizer_usernames:
-        organizer_headings += f"<h2>{username}</h2>"
-    
-    ###return organizer_headings
-    return render_template("user_organizers.html")
-
-# Get all organizers that have/had upcoming/past events
-def get_organizers():
-    
-    # Query the database to get a list of all organizer usernames with upcoming/past events
-    organizers = OrganizerEventDetails.query.with_entities(distinct(OrganizerEventDetails.organizer_username)).all()
-    
-    # Extracting the usernames from the query results
-    organizer_usernames = [organizer[0] for organizer in organizers]
-    
-    return organizer_usernames"""
-
-from app.main import db
-
 @user.route("/user/organizers", methods=["GET"])
 @login_required
 @user_required
 def view_organizers():
-    organizer_usernames = get_distinct_organizers()
-    return render_template("user_organizers.html", organizer_usernames=organizer_usernames)
+    organizers = get_active_organizers()
+    return render_template("user_organizers.html", organizers=organizers)
 
-def get_distinct_organizers():
-    # Join OrganizerEventDetails and EventDetails tables
-    query = (
-        db.session.query(OrganizerEventDetails.organizer_username)
-        .join(EventDetails, OrganizerEventDetails.event_id == EventDetails.id)
-        .distinct()
-    )
+# Get only the organizers that have upcoming events or had past events
+def get_active_organizers():
 
-    # Execute the query and get distinct organizer usernames
-    organizer_usernames = [result[0] for result in query.all()]
+    # Joining EventDetails and Credentials tables
+    # Selecting only distinct organizer usernames and names
+    organizers = db.session.query(Credentials.username, Credentials.name).join(
+        EventDetails, Credentials.username == EventDetails.organizer
+    ).distinct().all()
 
-    return organizer_usernames
+    return organizers
